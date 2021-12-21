1b9de3c0f10c2d5344e855a24514e819c927e6f2
Labbouyat <labbouyat@gmail.com>
2021-12-21 11:17:24 -0500

    exercise completed

---
+++ new app/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace new_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+     
+            string dogName = "Ralph";
+
+            int dogAge = 10;
+
+            Console.WriteLine($"My dog's name is {dogName}, He is {dogAge} years old");
+        }
+
+           
+    }
+}
